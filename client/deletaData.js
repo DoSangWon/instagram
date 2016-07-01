42054644e8c860d3e89cf723bad769cf05bc8953
cyc31041 <cyc31041@naver.com>
2016-07-01 15:19:02 +0900

    delete 구현완료

--- client/deletaData.js
+++ client/deletaData.js
@@ -5,10 +5,17 @@ Template.deleteData.events(
         "click #Dlt_btn": function (evt, tmpl) {
             evt.preventDefault();
             var obj = Session.get('selectedData') || {};
-            var _id = obj._id;
-            console.log(_id);
-            console.log("ddd");
-            console.log(Boards.remove({_id:_id}));
+            var user = obj.user;
+            console.log(user);
+            var c_user =  Meteor.user().emails[0].address;
+            console.log(c_user);
+            if(user == c_user) {
+                var _id = obj._id;
+                Boards.remove({_id: _id});
+            }
+            else{
+                alert("삭제 권한이 없습니다.");
+            }
         }
     }
 );
